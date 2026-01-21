fa87680818ca6f4af5eafff04c3dd85526c5571e
akshatraj01 <akshatraj1808@gmail.com>
2026-01-22 00:05:53 +0530

    Update Leetcode 206.cpp

---
+++ DAA_4_2025_26/Leetcode 206.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
+};
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+
+        while(curr!=NULL){
+            ListNode* nextNode=curr->next;
+            curr->next=prev;      
+            prev=curr;       
+            curr=nextNode;    
+        }
+        return prev;
+    }
+};
