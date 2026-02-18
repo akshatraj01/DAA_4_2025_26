20d6e5455703e1a30ef572d0723f4ab24118efec
akshatraj01 <akshatraj1808@gmail.com>
2026-02-19 00:12:14 +0530

    Add files via upload

---
+++ SESSION-18/Aggressive Cows.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+  public:
+    
+    bool canPlace(vector<int> &stalls,int k,int dist){
+        int count =1;                 
+        int lastPos =stalls[0];
+
+        for(int i=1;i<stalls.size(); i++){
+            if(stalls[i]-lastPos>=dist){
+                count++;
+                lastPos =stalls[i];
+            }
+            if(count >= k) return true;
+        }
+        return false;
+    }
+    
+    int aggressiveCows(vector<int> &stalls,int k){
+        
+        sort(stalls.begin(),stalls.end());   
+        
+        int n =stalls.size();
+        int low =1;
+        int high =stalls[n-1]-stalls[0];
+        int ans =0;
+        
+        while(low<=high){
+            int mid =low+(high-low)/2;
+            
+            if(canPlace(stalls,k,mid)){
+                ans =mid;        
+                low =mid+1;    
+            }else{
+                high=mid-1;   
+            }
+        }
+        return ans;
+    }
+};
